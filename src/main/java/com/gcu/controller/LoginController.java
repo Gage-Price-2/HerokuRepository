7b8778361d264bb37bfc983ffa5592199661c201
Gage-Price-2 <gprice11@my.gcu.edu>
2024-10-02 14:04:11 -0700

    AWS application properties

--- src/main/java/com/gcu/controller/LoginController.java
+++ src/main/java/com/gcu/controller/LoginController.java
@@ -45,8 +45,9 @@ public class LoginController {
 		}
 		
 		//Create some orders
-		List<OrderModel> orders = new ArrayList<OrderModel>();
-		orders.add(new OrderModel(1, "0000000000", "This is Product 1", 1, 1));
+		//List<OrderModel> orders = new ArrayList<OrderModel>();
+		
+		/*orders.add(new OrderModel(1, "0000000000", "This is Product 1", 1, 1));
 		orders.add(new OrderModel(2, "0000000001", "This is Product 2", 2, 2));
 		orders.add(new OrderModel(3, "0000000002", "This is Product 3", 3, 3));
 		orders.add(new OrderModel(4, "0000000003", "This is Product 4", 4, 4));
@@ -56,7 +57,7 @@ public class LoginController {
 		orders.add(new OrderModel(8, "0000000007", "This is Product 8", 8, 8));
 		orders.add(new OrderModel(9, "0000000008", "This is Product 9", 9, 9));
 		orders.add(new OrderModel(10, "0000000009", "This is Product 10", 10, 10));
-		orders.add(new OrderModel(100, "0000000100", "This is Product 100", 100, 100));
+		orders.add(new OrderModel(100, "0000000100", "This is Product 100", 100, 100));*/
 		
 		model.addAttribute("title", "My Orders");
 		model.addAttribute("orders", service.getOrders());
